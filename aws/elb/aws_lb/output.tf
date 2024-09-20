8b2d7ef9e046c5a3c82e70a3e2355a8dc1a8e958
soojin <74chung@naver.com>
2024-09-20 16:28:43 +0900

    20240920-1628

--- aws/elb/aws_lb/output.tf
+++ aws/elb/aws_lb/output.tf
@@ -1,11 +1,11 @@
 # aws_lb
 #------------------------------------------------------------------
 output "lb_id" {
-  value = aws_lb.company_account_env_function_lb.id
+  value = aws_lb.company_service_env_function_lb.id
 }
 output "lb_arn" {
-  value = aws_lb.company_account_env_function_lb.arn
+  value = aws_lb.company_service_env_function_lb.arn
 }
 output "lb_name" {
-  value = aws_lb.company_account_env_function_lb.name
+  value = aws_lb.company_service_env_function_lb.name
 }
