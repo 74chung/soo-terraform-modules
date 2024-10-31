0d0ae66e8e5227b2938227392a4d6c15ed0e2101
soojin <74chung@naver.com>
2024-10-31 12:21:00 +0900

    20241031-1220

--- aws/elastic_beanstalk/aws_elastic_beanstalk_environment/output.tf
+++ aws/elastic_beanstalk/aws_elastic_beanstalk_environment/output.tf
@@ -9,3 +9,7 @@ output "eb_env_arn" {
 output "eb_env_name" {
   value = aws_elastic_beanstalk_environment.company_service_env_function_app_env.name
 }
+
+output "eb_load_balancers" {
+  value = aws_elastic_beanstalk_environment.company_service_env_function_app_env.load_balancers
+}
