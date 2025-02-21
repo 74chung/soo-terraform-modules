36e3d94099dede81a279c77c8badf8e23812ecb4
soojin <74chung@naver.com>
2025-02-21 15:07:55 +0900

    20250221-1507

--- aws/vpc/aws_subnet/output.tf
+++ aws/vpc/aws_subnet/output.tf
@@ -3,3 +3,6 @@
 output "subnet_id" {
   value = "${aws_subnet.company_service_env_function_subnet.*.id}"
 }
+output "subnet_arn" {
+  value = "${aws_subnet.company_service_env_function_subnet.*.arn}"
+}
